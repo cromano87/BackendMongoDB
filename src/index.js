9f6c03e01a5dd5240dfb86c563f1ba95cc142145
cromano87 <cromano@legops.com>
2024-04-30 21:58:45 -0500

    Modificado

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 // configuracion express y puert
 const app = express();
 const port = 5000;
-app.use(express.json());
-app.use('/api/clientes', require('../routes/RoutesCliente'));
-app.use('/api/estudiantes', require('../routes/RoutesEstudiante'));
 
 // enlazamos la conexion a la base de datos
 conectarBD();
 app.use(cors());
+app.use(express.json());
 
+app.use('/api/clientes', require('../routes/RoutesCliente'));
+app.use('/api/estudiantes', require('../routes/RoutesEstudiante'));
 
 app.listen(port, () => console.log("Listening on port http://localhost:5000/"));
 
